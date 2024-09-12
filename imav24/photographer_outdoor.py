353b8b88d140f3eeae8050063fc8340991f87fe0
AnaBarcenas <ana.barcenas.dk@gmail.com>
2024-09-12 15:53:09 +0000

    modified photographer_outdoor.py

--- imav24/photographer_outdoor.py
+++ imav24/photographer_outdoor.py
@@ -9,7 +9,7 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 from px4_msgs.msg import VehicleGlobalPosition
 from std_msgs.msg import Empty, Float32, Bool, String
-#from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image
 
 from cv_bridge import CvBridge 
 
